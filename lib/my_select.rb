446b43e9d1767bed028b54e366680f422ed9fabf
Eric Hasselbring <ehasselbring@gmail.com>
2016-11-17 18:24:20 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+
+    array = []
+    index = 0
+    while index < collection.length
+        if yield(collection[index])
+            array << collection[index]
+        end
+        index +=1
+    end
+    return array
 end
